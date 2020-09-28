f8c201d2dd056c14ce2d994f9826713898ebadda
Senendra <senendra.007@gmail.com>
2020-09-28 17:36:30 +0530

    [Senendra] Add: Welcome to Line Comparision program

---
+++ LineCompare/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace LineCompare
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Line Comparision Computation Program");
+            
+        }
+    }
+}
